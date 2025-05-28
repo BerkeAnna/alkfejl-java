a80d24a9f04a8ddbeed4b559511a004fb0a781d2
BerkeAnna <berke.anna0803@gmail.com>
2025-05-28 12:37:04 +0200

    first

---
+++ first-maven/src/main/java/org/example/first_maven/App.java
@@ -0,0 +1,10 @@
+package org.example.first_maven;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
